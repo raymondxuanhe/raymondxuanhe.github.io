dea24760d19eda7f9aaa3ae68d13fd6040e0f5c7
Raymond <raymondhe@proton.me>
2025-08-17 21:09:01 -0500

    got the drafts off the ground

---
+++ files/jupyter_notebooks/time_iteration.jl
@@ -0,0 +1,91 @@
+
+
+using Interpolations, LinearAlgebra, Roots, Plots
+
+const β = 0.96
+const α = 0.4
+
+const kmin = 1e-3;
+const kmax = 100.0;
+const nk = 1_001;
+const kgrid = range(kmin, kmax, length=nk)
+
+f(k) = k^α
+u(c) = c ≥ 0.0 ? log(c) : -1e10
+u_c(c) = c ≥ 0.0 ? 1/c : 0.0
+f_k(k) = α * k^(α - 1) 
+
+function coleman_operator(polguess_V::Vector{Float64})
+    polguess_F = CubicSplineInterpolation(kgrid, polguess_V)
+    Kp_V = zeros(nk)
+    for i in eachindex(kgrid)
+        k = kgrid[i]
+        function euler_eq(k′)
+            LHS = u_c(f(k) - k′)
+            c′ = f(k′) - polguess_F(k′)
+            RHS = β*f_k(k′)*u_c(c′)
+            return LHS - RHS
+        end
+        Kp_V[i] = find_zero(euler_eq, (kmin, f(k)))
+    end
+    return Kp_V
+end
+
+function solve_model_coleman()
+    polguess_V = zeros(nk)
+    iiter = 0
+    diff = 1
+    max_iter = 600
+    tol = 1e-8
+
+    @time while iiter < max_iter 
+        iiter += 1
+        polnew_V = coleman_operator(polguess_V)
+
+        diff = norm(polnew_V - polguess_V, Inf)
+
+#        println("Iteration $iiter, Error: $diff")
+
+        if diff < tol
+            println("Converged after $iiter iterations")
+            break
+        end
+
+        polguess_V = polnew_V
+    end
+    return polguess_V
+end
+
+pol_V = solve_model_coleman()
+
+using Plots
+
+plot(kgrid, pol_V, label="Coleman Time Iteration", 
+    xlabel="Capital", ylabel="Policy Function", 
+    title="Policy Function from Coleman Time Iteration")
+
+pol_analytical(k) = α*β*k^α
+
+plot!(kgrid, pol_analytical.(kgrid), label="Analytical Policy Function", 
+    linestyle=:dash, color=:red)
+
+pol_F = CubicSplineInterpolation(kgrid, polguess_V)
+
+function compute_euler_residual(k::Float64)
+    k′ = pol_F(k)
+    c = f(k) - k′
+    LHS = u_c(c)
+    k′′ = pol_F(k′)
+    c′ = f(k′) - k′′
+    RHS = β * f_k(k′) * u_c(c′)
+    return log10(abs(LHS/RHS - 1))
+end
+
+kfine_V = range(kmin, kmax, length=5_001)
+
+errors_V = compute_euler_residual.(kfine_V)
+
+#histogram(errors_V)
+using Statistics
+
+mean(errors_V)
