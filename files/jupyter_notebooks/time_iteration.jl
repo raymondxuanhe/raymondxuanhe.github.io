894575716e96ad449650bacfad3a6e6e51684023
Raymond <raymondhe@proton.me>
2025-08-29 13:05:23 -0500

    major improvement in documentation

--- files/jupyter_notebooks/time_iteration.jl
+++ files/jupyter_notebooks/time_iteration.jl
@@ -1,6 +1,6 @@
-
-
 using Interpolations, LinearAlgebra, Optim, Plots, Roots
+using Parameters
+
 
 const β = 0.96
 const α = 0.4
@@ -13,24 +13,8 @@ const kgrid = range(kmin, kmax, length=nk)
 f(k) = k^α
 u(c) = c ≥ 0.0 ? log(c) : -1e10
 u_c(c) = c ≥ 0.0 ? 1/c : 0.0
-f_k(k) = α * k^(α - 1) 
-
-function coleman_operator_optim(polguess_V::Vector{Float64})
-    polguess_F = CubicSplineInterpolation(kgrid, polguess_V)
-    Kp_V = zeros(nk)
-    for i in eachindex(kgrid)
-        k = kgrid[i]
-        function euler_eq(k′)
-            LHS = u_c(f(k) - k′)
-            c′ = f(k′) - polguess_F(k′)
-            RHS = β*f_k(k′)*u_c(c′)
-            return LHS - RHS
-        end
-        results = optimize(k′ -> euler_eq(k′)^2, kmin, f(k))
-        Kp_V[i] = results.minimizer
-    end
-    return Kp_V
-end
+f_k(k) = α * k^(α - 1)
+u_c_inv(c) = c ≥ 0.0 ? 1/c : -1e10
 
 function coleman_operator_roots(polguess_V::Vector{Float64})
     polguess_F = CubicSplineInterpolation(kgrid, polguess_V)
@@ -57,7 +41,7 @@ function solve_model_coleman()
 
     @time while iiter < max_iter 
         iiter += 1
-        polnew_V = coleman_operator_optim(polguess_V)
+        polnew_V = coleman_operator_roots(polguess_V)
 
         diff = norm(polnew_V - polguess_V, Inf)
 
@@ -104,3 +88,40 @@ errors_V = compute_euler_residual.(kfine_V)
 using Statistics
 
 mean(errors_V)
+
+function compute_euler_residual_improved(k::Float64)
+    k′ = pol_F(k)
+    c = f(k) - k′
+    k′′ = pol_F(k′)
+    c′ = f(k′) - k′′
+    RHS = β * f_k(k′) * u_c(c′)
+    return log10(abs(1 - (u_c_inv(RHS)/c)))
+end
+
+errors_V_improved = compute_euler_residual_improved.(kfine_V)
+
+#histogram(errors_V)
+using Statistics
+
+mean(errors_V)
+
+
+
+
+function coleman_operator_optim(polguess_V::Vector{Float64})
+    polguess_F = CubicSplineInterpolation(kgrid, polguess_V)
+    Kp_V = zeros(nk)
+    for i in eachindex(kgrid)
+        k = kgrid[i]
+        function euler_eq(k′)
+            LHS = u_c(f(k) - k′)
+            c′ = f(k′) - polguess_F(k′)
+            RHS = β*f_k(k′)*u_c(c′)
+            return LHS - RHS
+        end
+        results = optimize(k′ -> euler_eq(k′)^2, kmin, f(k))
+        Kp_V[i] = results.minimizer
+    end
+    return Kp_V
+end
+
